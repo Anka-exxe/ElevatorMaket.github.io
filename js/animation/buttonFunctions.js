31b87a8018bcf78af2373a11c1d4cadfecb9f49d
Anka-exxe <anna5437743@mail.ru>
2025-02-03 06:37:22 +0300

    Created maket

---
+++ js/animation/buttonFunctions.js
@@ -0,0 +1,16 @@
+function selectWall(button) {
+    selectParameterButton(button);
+}
+
+function selectParameterButton(button) {
+    const container = button.parentNode; 
+    const buttons = container.querySelectorAll('button'); 
+    buttons.forEach(btn => {
+        btn.classList.remove('active'); 
+    });
+    button.classList.add('active'); 
+}
+
+function selectManyParameterButton(button) {
+    button.classList.toggle('active');
+}
---
+++ js/animation/tabFunctions.js
@@ -0,0 +1,140 @@
+const tabImageMap = new Map([
+    ['MainParametersTab', 'main_parameters_icon'],
+    ['WallsParametersTab', 'wall_icon'],
+    ['DoorParametrsTab', 'doors_icon'],
+    ['CeilingParametrsTab', 'ceiling_icon'],
+    ['FloorParametrsTab', 'floor_icon'],
+    ['BoardParametrsTab', 'board_icon'],
+    ['MirrorParametrsTab', 'mirror_icon'],
+    ['HandrailParametrsTab', 'handrail_icon'],
+    ['OtherParametrsTab', 'other_icon']
+]);
+
+function showTab(tabId) {
+    const tabs = document.querySelectorAll('.menu-container__content');
+    const navigationTabs = document.querySelectorAll('.navigation__tab');
+    const icons = document.querySelectorAll('.navigation--with-animation__animation-picture');
+
+    tabs.forEach(tab => {
+        tab.classList.remove('active');
+    });
+
+    navigationTabs.forEach(tab => {
+        tab.classList.remove('active');
+    });
+
+    icons.forEach(icon => {
+        icon.classList.remove('active');
+    });
+
+    document.getElementById(tabId).classList.add('active');
+
+    const activeTab = Array.from(navigationTabs).find(tab => tab.innerText === document.getElementById(tabId).querySelector('.menu-container__options-menu-title').innerText);
+    if (activeTab) {
+        activeTab.classList.add('active');
+    }
+
+    const iconId = tabImageMap.get(tabId);
+    const activeIcon = document.getElementById(iconId);
+    if (activeIcon) {
+        activeIcon.classList.add('active');
+    }
+
+    let category = '';
+    switch (tabId) {
+        case 'WallsParametersTab':
+            category = 'walls';
+            break;
+        case 'CeilingParametrsTab':
+            category = 'ceiling';
+            break;
+        case 'FloorParametrsTab':
+            category = 'floor';
+            break;
+        case 'BoardParametrsTab':
+            category = 'board';
+            break;
+        case 'DoorParametrsTab':
+            category = 'door';
+            break;
+        case 'OtherParametrsTab':
+            category = 'floor'; // Добавьте нужную категорию
+            break;
+    }
+
+    // Показываем изображения для выбранной категории
+    showImages(category, tabId);
+}
+
+const images = {
+    walls: [
+        '/Стены/5024.jpg', 
+        '/Стены/DL16CE_diffuse.jpg',
+        '/Стены/DL16CE_gray.jpg',
+        '/Стены/DL16CE_normal.jpg',
+        '/Стены/DL16CE_roughness.jpg',
+        '/Стены/DL89E_diffuse.jpg',
+        '/Стены/DL89E_glossiness.jpg',
+        '/Стены/DL89E_gray.jpg',
+        '/Стены/DL89E_normal.jpg',
+        '/Стены/HL Ti-Gold.jpg',
+        '/Стены/HY-003 TI-GOLD.jpg',
+        '/Стены/HY-004 TI-GOLD.jpg',
+        '/Стены/RAL-7035-Svetlo-serii.png',
+        '/Стены/шлифованная нержавейка.jpg',
+    ],
+    ceiling: [
+        '/Потолок/00.png', 
+        '/Потолок/01.jpg', 
+        '/Потолок/1.jpg', 
+        '/Потолок/2.jpg',
+        '/Потолок/6.png',
+        '/Потолок/07.jpg',
+        '/Потолок/7.png',
+        '/Потолок/8.png',
+        '/Потолок/9.png',
+        '/Потолок/11.png',
+        '/Потолок/21.jpg',
+        '/Потолок/P23.jpg',
+        '/Потолок/Безымянный-2.png',
+        '/Потолок/Р04.jpg',
+    ],
+    floor: [
+        '/Пол/nero marquina.jpg', 
+        '/Пол/WoodFlooring042_COL_4K.jpg',  
+        '/Пол/basalt-grey.jpg',  
+        '/Пол/antik2.png', 
+        '/Пол/WoodFlooring042_DISP_4K.jpg',
+    ],
+    board: [
+        '/Табло/TL-D70.png', 
+        '/Табло/BUTSAN TABLO.jpg',
+        '/Табло/BUTSAN1-01.jpg', 
+        '/Табло/BUTSAN1-01111.jpg',        
+    ],
+    door: [
+        '/Двери/HL Ti-Gold.jpg',
+        '/Двери/HY-003 TI-GOLD.jpg', 
+        '/Двери/HY-004 TI-GOLD.jpg',
+        '/Двери/RAL-7035-Svetlo-serii.png',
+        '/Двери/ral9016.jpg',
+        '/Двери/шлифованная нержавейка.jpg',
+    ]
+};
+
+function showImages(category, tabId) {
+    const parentElement = document.getElementById(tabId); // Замените на ваш класс родителя
+    const container = parentElement.querySelector('.menu-container__options-menu-container-main .textures-container'); 
+ 
+    container.innerHTML = ''; // Очищаем предыдущие изображения
+
+    if (images[category]) {
+        images[category].forEach(imagePath => {
+            const img = document.createElement('img');
+            img.src = imagePath;
+            img.alt = `${category} image`;
+            img.className = 'texture-image';
+            container.appendChild(img);
+        });
+    }
+}
