4a9892dfea1d1529618aa723fbd33c425823a5a5
Anka <anna5437743@mail.ru>
2025-05-06 21:28:39 +0300

    changed host url

--- AdminPanel/js/urlHelper/urls.js
+++ AdminPanel/js/urlHelper/urls.js
@@ -10,7 +10,7 @@ export const BUMPER = 'BUMPER';
 export const API_BASE_URL = window.location.origin;
 export const MODEL_BASE_PATH = `${API_BASE_URL}/models/`;
 
-export const host = "http://localhost:8090/api/v1/";
+const host = `${API_BASE_URL}/api/v1/`;
 export const urlTemplateGetIcons = `${host}icons?page=0&size=1000`;
 export const urlTemplateGetTextureByIconId = `${host}textures/icon/{}`;
 
